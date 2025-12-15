fdc6ec1e4b2f596762a170d8f4821a8464369aa6
Wazih-Taosif <haq.wazih@gmail.com>
2025-12-15 10:51:02 -0500

    did the backend for trhe register page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, flash, redirect
 
 import pymysql
 
@@ -8,6 +8,8 @@ from dynaconf import Dynaconf
 app = Flask(__name__)
 
 config = Dynaconf(settings_file = ["settings.toml"])
+
+app.secret_key = config.secret_key
 
 def connect_db():
     conn = pymysql.connect(
@@ -58,6 +60,25 @@ def product_page(product_id):
 def login():
     return render_template("login.html.jinja")
 
-@app.route("/register")
+@app.route("/register", methods = ["POST", "GET"])
 def register():
+    if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        address = request.form["address"]
+
+        if password != confirm_password:
+            flash("Passwords do not match")
+        elif len(password) < 8:
+            flash("Password is too short")
+        else:
+            connection = connect_db()
+            cursor = connection.cursor()
+            cursor.execute("""
+                INSERT INTO `User` (`Name`, `Password`, `Email`, `Address`)
+                VALUES (%s, %s, %s, %s)
+            """, (name, password, email, address))
+            return redirect('/login')
     return render_template("register.html.jinja")
